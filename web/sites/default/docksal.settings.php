a29ebcecd2aaaae8b4bc27c998dadf7cb9b7334b
Oliver Davies <oliver@oliverdavies.uk>
2018-08-31 09:01:22 +0100

    Update variable name

--- web/sites/default/docksal.settings.php
+++ web/sites/default/docksal.settings.php
@@ -3,7 +3,7 @@
 $databases['default']['default'] = [
   'driver' => 'mysql',
   'host' => 'db',
-  'database' => getenv('MYSQL_DB'),
+  'database' => getenv('MYSQL_DATABASE'),
   'username' => getenv('MYSQL_USER'),
   'password' => getenv('MYSQL_PASSWORD'),
 ];
